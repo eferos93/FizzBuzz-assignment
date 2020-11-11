f12384e46e1cd3a4170e4dd2b036ebcac6a77605
Eros Fabrici <eros.fabrici@gmail.com>
2020-11-11 16:03:39 +0100

    Assignment done

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
@@ -1,9 +1,49 @@
 package dssc.assignment.fizzbuzz;
 
+import java.time.Instant;
+import java.util.stream.IntStream;
+
 public class FizzBuzz {
+    static public void play(Integer number) {
+        IntStream.rangeClosed(1, number)
+                .forEach(x -> {
+                    StringBuilder test = new StringBuilder();
+                    if (x % 3 == 0) {
+                        test.append("Fizz");
+                    }
+                    if (x % 5 == 0) {
+                        test.append("Buzz");
+                    }
+                    System.out.println(test.isEmpty() ? x : test);
+                });
+    }
+
+    static public void playExtendedVersion(Integer number) {
+        IntStream.rangeClosed(1, number)
+                .forEach(x -> {
+                    StringBuilder test = new StringBuilder();
+                    if(x % 3 == 0) {
+                        test.append("Fizz");
+                    }
+
+                    if (x % 5 == 0) {
+                        test.append("Buzz");
+                    }
+
+                    if (x % 7 == 0) {
+                        test.append("Bang");
+                    } else {
+                        test = new StringBuilder(); //set to empty
+                    }
+                    System.out.println(test.isEmpty() ? x : test);
+                });
+    }
+
 
     public static void main(String... args) {
-        
+        Integer n = 105;
+        playExtendedVersion(105);
+
     }
 
 }
