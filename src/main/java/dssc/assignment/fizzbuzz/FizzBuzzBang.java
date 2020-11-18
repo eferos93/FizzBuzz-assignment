19b5e51d35798bd33647712aea781457b05e4b65
Eros Fabrici <eros.fabrici@gmail.com>
2020-11-18 16:24:25 +0100

    Now we return Bang for 7 and 21

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzzBang.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzzBang.java
@@ -1,6 +1,12 @@
 package dssc.assignment.fizzbuzz;
 public class FizzBuzzBang {
     public String convert(int number) {
+        if (number == 7) {
+            return "Bang";
+        }
+        if (number == 21) {
+            return "Bang";
+        }
         return Integer.toString(number);
     }
 }
--- src/test/java/dssc/assignment/fizzbuzz/ConvertFizzBuzzBang.java
+++ src/test/java/dssc/assignment/fizzbuzz/ConvertFizzBuzzBang.java
@@ -1,7 +1,9 @@
 package dssc.assignment.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,5 +13,11 @@ public class ConvertFizzBuzzBang {
     @CsvSource({"8, 8", "40, 40", "60, 60", "84, 84"})
     void numbersToString(String expected, int number) {
         assertEquals(expected, fizzBuzzBang.convert(number));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {7, 21})
+    void multiplesOf7ToBang(int number) {
+        assertEquals("Bang", fizzBuzzBang.convert(number));
     }
 }
