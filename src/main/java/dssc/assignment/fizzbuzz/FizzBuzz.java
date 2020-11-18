57b01a84f7fbb1798871e2184cbe3083a79a3f20
Eros Fabrici <eros.fabrici@gmail.com>
2020-11-18 15:59:44 +0100

    complete FizzBuzz

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
@@ -5,42 +5,6 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class FizzBuzz {
-    static public void play(Integer number) {
-        IntStream.rangeClosed(1, number)
-                .forEach(x -> {
-                    StringBuilder test = new StringBuilder();
-                    if (x % 3 == 0) {
-                        test.append("Fizz");
-                    }
-                    if (x % 5 == 0) {
-                        test.append("Buzz");
-                    }
-                    System.out.println(test.isEmpty() ? x : test);
-                });
-    }
-
-    static public void playExtendedVersion(Integer number) {
-        IntStream.rangeClosed(1, number)
-                .forEach(x -> {
-                    StringBuilder test = new StringBuilder();
-                    if(x % 3 == 0) {
-                        test.append("Fizz");
-                    }
-
-                    if (x % 5 == 0) {
-                        test.append("Buzz");
-                    }
-
-                    if (x % 7 == 0) {
-                        test.append("Bang");
-                    } else {
-                        test = new StringBuilder(); //set to empty
-                    }
-                    System.out.println(test.isEmpty() ? x : test);
-                });
-    }
-
-
     public String convert(int number) {
         if (number % 15 == 0) {
             return "FizzBuzz";
